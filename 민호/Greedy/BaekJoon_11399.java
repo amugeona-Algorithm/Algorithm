aa248be084c0eb39f78840a752cf00a390873905
mino <koho104777@gmail.com>
2023-07-05 03:01:01 +0900

    ATM 문제

---
+++ 민호/Greedy/BaekJoon_11399.java
@@ -0,0 +1,37 @@
+package 민호.Greedy;
+
+import java.util.*;
+import java.io.*;
+
+public class BaekJoon_11399 {
+    /**
+     * 백준 1931
+     * Greedy 회의실 배정
+     */
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> pArray = new ArrayList<>();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            pArray.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(pArray);
+        int result = 0;
+
+        for (int j = 0; j < pArray.size(); j++) {
+            for(int k=0; k<=j; k++){
+                result += pArray.get(k);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
