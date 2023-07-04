9ae4873d1e227a6675fc621c6c51e3a9df64631e
JunSeok Kim <skaksdl1238@gmail.com>
2023-07-04 13:56:34 +0900

    feat - 백준 1931

--- 준석/week1/BJ2075.java
+++ 준석/week1/BJ2075.java
@@ -8,23 +8,21 @@ import java.util.stream.Collectors;
 
 //https://www.acmicpc.net/problem/2075
 public class BJ2075 {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static List<Integer> splitInputNumber(int size) throws IOException {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         List<String> copyString = new ArrayList<>();
+        int size = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < size; i++) {
             Collections.addAll(copyString, br.readLine().split(" "));
         }
-        return copyString.stream().map(Integer::parseInt)
+        List<Integer> a = copyString.stream()
+                .map(Integer::parseInt)
                 .sorted(Collections.reverseOrder())
                 .collect(Collectors.toList());
-    }
 
-    public static void main(String[] args) throws IOException {
-
-        int size = Integer.parseInt(br.readLine());
-
-        System.out.println(splitInputNumber(size).get(size - 1));
+        System.out.println(a.get(size - 1));
     }
 }
