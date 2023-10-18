342370230a0d4b3005eb42df3b91b92ed2c43ef6
최지환 <cjh87467@gmail.com>
2023-10-18 15:02:53 +0900

    문제

---
+++ 지환/w14/PRO_67257.java
@@ -0,0 +1,108 @@
+package 지환.w14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class PRO_67257 {
+    public static long result = Long.MIN_VALUE;
+
+    public static List<Long> numbers = new ArrayList<>();
+    public static List<String> symbolList = new ArrayList<>();
+
+    public static String[] symbols = {"+", "-", "*"};
+    public static String[] output = new String[3];
+    public static boolean[] visited = new boolean[3];
+
+
+    public long solution(String expression) {
+        String n = "";
+
+
+        for (int i = 0; i < expression.length(); i++) {
+            char exp = expression.charAt(i);
+            if (exp == '+' || exp == '-' || exp == '*') {
+                symbolList.add(exp + "");
+                numbers.add(Long.parseLong(n));
+                n = "";
+            } else {
+                n += exp;
+            }
+        }
+
+        //마지막 숫자 넣기
+        numbers.add(Long.parseLong(n));
+
+        per(0, symbols.length);
+        return result;
+    }
+
+    static void per(int depth, int r){
+        if(depth == r){
+            solve();
+            return;
+        }
+        for (int i = 0; i < symbols.length; i++) {
+            if(!visited[i]){
+                visited[i]=true;
+                output[depth] = symbols[i];
+                per(depth + 1, r);
+                visited[i] = false;
+            }
+        }
+    }
+    static void solve(){
+        //우선순위로 저장해둔 연산자들을 리스트에 넣어줌
+        List<String> tempSymbols = new ArrayList<>();
+        tempSymbols.addAll(symbolList);
+
+        List<Long> num = new ArrayList<>();
+        num.addAll(numbers);
+
+        for (int i = 0; i < output.length; i++) {
+            String curOper = output[i];//현재 우선순위 연산자
+
+            for (int j = 0; j < tempSymbols.size(); j++) {
+                //현재 우선순위에 맞는 연산자인 경우
+                if(tempSymbols.get(j).equals(curOper)){
+                    long n1 = num.get(j);
+                    long n2 = num.get(j + 1);
+                    long tempResult = cal(n1, n2, curOper);
+
+                    //숫자 제거
+
+                    num.remove(j + 1);
+                    num.remove(j);
+
+                    //사용한 연산자 제거
+                    tempSymbols.remove(j);
+
+                    num.add(j, tempResult);
+
+                    j--;// 연산자를 삭제 했으니 인덱스 하나 줄여서 반복문 사이즈에 맞게 돌도락함.
+                }
+
+            }
+
+        }
+        result = Math.max(result, Math.abs(num.get(0)));
+    }
+
+    static long cal(long n1, long n2, String o) {
+        long result = 0;
+        switch(o) {
+            case "+" :
+                result = n1 + n2;
+                break;
+            case "-" :
+                result = n1 -n2;
+                break;
+            case "*" :
+                result = n1 * n2;
+                break;
+        }
+
+        return result;
+
+    }
+
+}
---
+++ 지환/w14/PRO_72411.java
@@ -0,0 +1,87 @@
+package 지환.w14;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class PRO_72411 {
+    public String[] solution(String[] orders, int[] course) {
+        List<Set<String>> orderList = Arrays.stream(orders)
+                .map(String::chars)
+                .map(charStream -> charStream.mapToObj(menu -> String.valueOf((char) menu)).collect(Collectors.toSet()))
+                .collect(Collectors.toList());
+
+        Map<Integer, List<Course>> courses = new HashMap<>();
+        for (int length : course) {
+            List<Course> list = new ArrayList<>();
+            list.add(new Course("", 0));
+            courses.put(length, list);
+        }
+        getCourses('A', new HashSet<>(), orderList, courses);
+        return courses.values().stream()
+                .filter(list -> list.get(0).occurrences > 0)
+                .flatMap(List::stream)
+                .map(c -> c.course)
+                .sorted()
+                .toArray(String[]::new);
+
+    }
+
+    //재
+    private void getCourses(char nextMenu, Set<String> selectedMenus,
+                            List<Set<String>> orderList,
+                            Map<Integer, List<Course>> courses) {
+        // 주문 중 현재까지 구한 메뉴 조합의 등장 횟수 확인.
+        int occurrences = (int) orderList.stream()
+                .filter(order -> order.containsAll(selectedMenus))
+                .count();
+        //등장 횟수가 2 미만인 경우 리턴
+        if (occurrences < 2) return;
+
+        int size = selectedMenus.size();
+
+        //코스중 해당 크키만큼의 코스가 있으면
+        if (courses.containsKey(size)) {
+            //그 코스 꺼내서
+            List<Course> courseList = courses.get(size);
+            //코스 생성
+            Course course = new Course(selectedMenus.stream()
+                    .sorted()
+                    .collect(Collectors.joining("")),
+                    occurrences);
+
+            Course original = courseList.get(0);
+
+
+            // 기존 것 보다 중복 횟수가 더 많으면 초기화하고 추가
+            if (original.occurrences < occurrences) {
+                courseList.clear();
+                courseList.add(course);
+            } // 같은 경우  그냥 추가
+            else if (original.occurrences == occurrences) {
+                courseList.add(course);
+            }
+        }
+
+        //메뉴가 10개 이상이면 끝
+        if (size >= 10) return;
+
+        for (char menuChar = nextMenu; menuChar <= 'Z'; menuChar++) {
+            String menu = String.valueOf(menuChar);
+            selectedMenus.add(menu);
+            getCourses((char) (menuChar + 1), selectedMenus, orderList, courses);
+            selectedMenus.remove(menu);
+        }
+    }
+
+    private static class Course {
+        public final String course;
+        public final int occurrences;
+
+        public Course(String course, int occurrences) {
+            this.course = course;
+            this.occurrences = occurrences;
+        }
+
+    }
+
+}
